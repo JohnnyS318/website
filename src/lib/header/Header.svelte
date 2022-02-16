adefdd233b8c231127c0da58e4019069db494a14
Jonas Schneider <jonas.max.schneider@gmail.com>
2022-02-17 00:30:26 +0100

    Header updates

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -3,18 +3,12 @@
 	import logo from './svelte-logo.svg';
 </script>
 
-<header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
-
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
+<header class="flex absolute w-full justify-center items-center">
+	<nav class="nav grid items-center justify-center bg-gray-200 h-full px-12 rounded-b-xl" style="grid-template-columns: auto 1fr;">
+		<div class="">
+			<h3 class="font-sans text-2xl py-3 px">Jonas Schneider</h3>
+		</div>
+		<ul class="nav-list list-none flex flex-row items-center justify-center ml-16 text-black">
 			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
 			<li class:active={$page.url.pathname === '/about'}>
 				<a sveltekit:prefetch href="/about">About</a>
@@ -26,9 +20,6 @@
 				<a href="/blog">Blog</a>
 			</li>
 		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
 	</nav>
 
 	<div class="corner">
@@ -36,95 +27,14 @@
 	</div>
 </header>
 
-<style>
-	header {
-		display: flex;
-		justify-content: space-between;
-		position: absolute;
-		top: 0;
-		width: 100%;
-	}
+<style lang="postcss">
 
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--primary-color: #eeeeee;
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--primary-color);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--primary-color);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li.active::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--accent-color);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 1em;
-		color: var(--heading-color);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--accent-color);
-	}
+.nav{
+	border-color: black;
+	border: solid 1px;
+	border-top: 0px;
+}
+.nav-list li{
+	 
+}
 </style>
